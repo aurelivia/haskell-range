2cfe8840f3cbe7330c2f92ebc0cafe5c2b56e780
Olivia Taliesin <olivia@taliesin.dev>
2023-12-28 17:23:48 +0100

    Unions

--- Data/Range.hs
+++ Data/Range.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Data.Range
   -- * Datatypes
   ( Range(rangeLength)
@@ -5,9 +7,11 @@ module Data.Range
   , empty, singleton, range
   -- * Queries
   , contains
+  -- * Operations
+  , union
   ) where
 
-import Data.List (intercalate)
+import Data.List (intercalate, foldl', sort)
 
 data Span a = Span
   { lowerBound :: a
@@ -15,14 +19,44 @@ data Span a = Span
   , spanLength :: Int
   } deriving (Eq)
 
+instance Ord a => Ord (Span a) where
+  compare (Span xlb xub _) (Span ylb yub _) = if
+    | xlb < ylb -> LT
+    | ylb < xlb -> GT
+    | xub < yub -> LT
+    | yub < xub -> GT
+    | otherwise -> EQ
+
 spanOf :: (Ord a, Enum a) => a -> a -> Span a
 spanOf lb ub
   | lb > ub   = spanOf ub lb
   | otherwise = Span lb ub ((fromEnum ub) - (fromEnum lb))
 
-inSpan :: (Ord a) => a -> Span a -> Bool
+inSpan :: Ord a => a -> Span a -> Bool
 inSpan x (Span lb ub _) = x >= lb && x <= ub
 {-# INLINE inSpan #-}
+
+spanContains :: Ord a => Span a -> Span a -> Bool
+spanContains (Span xlb xub _) (Span ylb yub _) = xlb >= ylb && xlb <= yub
+  && xub >= ylb && xub <= yub
+
+spanOverlaps :: Ord a => Span a -> Span a -> Bool
+spanOverlaps (Span xlb xub _) (Span ylb yub _) = (xlb >= ylb && xlb <= yub)
+  || (xub >= ylb && xub <= yub)
+
+mergeSpans :: (Ord a, Enum a) => Span a -> Span a -> Span a
+mergeSpans (Span xlb xub _) (Span ylb yub _) = spanOf (min xlb ylb) (max xub yub)
+
+unionSpan :: (Ord a, Enum a) => [Span a] -> [Span a]
+unionSpan = unionSpan' . sort where
+  unionSpan' [] = []
+  unionSpan' (x:[]) = [x]
+  unionSpan' (x:xs@(y:ys)) = if
+    | spanContains x y -> unionSpan' xs
+    | spanOverlaps x y -> unionSpan' $ (mergeSpans x y) : ys
+    | otherwise        -> x : unionSpan' xs
+
+
 
 
 
@@ -54,6 +88,15 @@ range lb ub = let l = (fromEnum ub) - (fromEnum lb) in Range [Span lb ub l] l
 
 
 -- | Checks whether a given element falls within a given range
-contains :: (Ord a) => Range a -> a -> Bool
+contains :: Ord a => Range a -> a -> Bool
 contains (Range sps _) x = any (\s -> inSpan x s) sps
 {-# INLINE contains #-}
+
+
+
+
+
+-- | Creates a new range consiting of a union of two ranges.
+union :: (Ord a, Enum a) => Range a -> Range a -> Range a
+union (Range xsp _) (Range ysp _) = Range sp' (foldl' (\s -> \(Span _ _ l) -> s + l) 0 sp') where
+  sp' = unionSpan (xsp ++ ysp)
