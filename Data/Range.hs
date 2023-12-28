d39253f927d51b1f1acaed1fe66666f5c1545bd4
Olivia Taliesin <olivia@taliesin.dev>
2023-12-28 19:03:42 +0100

    getNth

--- Data/Range.hs
+++ Data/Range.hs
@@ -4,11 +4,13 @@ module Data.Range
   -- * Datatypes
   ( Range(rangeLength)
   -- * Range Constructors
-  , empty, singleton, range
+  , empty, singleton, range, fromList, include, includes
   -- * Queries
-  , contains
+  , within
   -- * Operations
-  , union
+  , union, unions
+  -- * Iteration
+  , getNth
   ) where
 
 import Data.List (intercalate, foldl', sort)
@@ -30,7 +32,7 @@ instance Ord a => Ord (Span a) where
 spanOf :: (Ord a, Enum a) => a -> a -> Span a
 spanOf lb ub
   | lb > ub   = spanOf ub lb
-  | otherwise = Span lb ub ((fromEnum ub) - (fromEnum lb))
+  | otherwise = Span lb ub ((+) 1 $ (fromEnum ub) - (fromEnum lb))
 
 inSpan :: Ord a => a -> Span a -> Bool
 inSpan x (Span lb ub _) = x >= lb && x <= ub
@@ -51,10 +53,23 @@ unionSpan :: (Ord a, Enum a) => [Span a] -> [Span a]
 unionSpan = unionSpan' . sort where
   unionSpan' [] = []
   unionSpan' (x:[]) = [x]
-  unionSpan' (x:xs@(y:ys)) = if
-    | spanContains x y -> unionSpan' xs
-    | spanOverlaps x y -> unionSpan' $ (mergeSpans x y) : ys
-    | otherwise        -> x : unionSpan' xs
+  unionSpan' (x@(Span _ xub _):xs@(y@(Span ylb _ _):ys)) = if
+    | spanContains x y  -> unionSpan' xs
+    | spanOverlaps x y  -> unionSpan' $ (mergeSpans x y) : ys
+    | (succ xub) == ylb -> unionSpan' $ (mergeSpans x y) : ys
+    | otherwise         -> x : unionSpan' xs
+
+getNthOfSpan :: (Ord a, Enum a) => Int -> Span a -> a
+getNthOfSpan 0 (Span lb _ _) = lb
+getNthOfSpan i (Span lb ub len)
+  | i == (len - 1)  = ub
+  | i > len         = error "Out of bounds."
+  | i <= mid        = getNth' succ i lb
+  | otherwise       = getNth' pred (i - mid) ub
+  where
+    mid = div ((fromEnum ub) - (fromEnum lb)) 2
+    getNth' _  0 x = x
+    getNth' fn i x = getNth' fn (i - 1) (fn x)
 
 
 
@@ -72,6 +87,13 @@ instance Show a => Show (Range a) where
   showsPrec i (Range sps _) = (++) $ '[' : (intercalate ", " sps') ++ "]" where
     sps' = map (\(Span lb ub _) -> (showsPrec i lb $ "..") ++ (showsPrec i ub $ "")) sps
 
+instance (Ord a, Enum a) => Semigroup (Range a) where
+  (<>) = union
+
+instance (Ord a, Enum a) => Monoid (Range a) where
+  mempty = empty
+
+
 -- | Constructs an empty range.
 empty :: Range a
 empty = Range [] 0
@@ -85,12 +107,26 @@ range :: (Ord a, Enum a) => a -> a -> Range a
 range lb ub = let l = (fromEnum ub) - (fromEnum lb) in Range [Span lb ub l] l
 {-# INLINE range #-}
 
+-- | Constructs a range from a list of values, where the resulting range covers only the values in the list.
+fromList :: (Ord a, Enum a) => [a] -> Range a
+fromList = unions . map singleton
+
+-- | Creates a new range covering an existing range plus an additional element
+include :: (Ord a, Enum a) => a -> Range a -> Range a
+include x r
+  | within x r = r
+  | otherwise  = union r $ singleton x
+
+-- | Creates a new range covering an existing range plus a set of elements
+includes :: (Ord a, Enum a, Foldable t) => t a -> Range a -> Range a
+includes xs r = foldl' (\r' -> \x -> include x r') r xs
+
 
 
 -- | Checks whether a given element falls within a given range
-contains :: Ord a => Range a -> a -> Bool
-contains (Range sps _) x = any (\s -> inSpan x s) sps
-{-# INLINE contains #-}
+within :: Ord a => a -> Range a -> Bool
+within x (Range sps _) = any (\s -> inSpan x s) sps
+{-# INLINE within #-}
 
 
 
@@ -100,3 +136,21 @@ contains (Range sps _) x = any (\s -> inSpan x s) sps
 union :: (Ord a, Enum a) => Range a -> Range a -> Range a
 union (Range xsp _) (Range ysp _) = Range sp' (foldl' (\s -> \(Span _ _ l) -> s + l) 0 sp') where
   sp' = unionSpan (xsp ++ ysp)
+
+-- | Creates a new range that is a union of a list of ranges.
+unions :: (Ord a, Enum a) => [Range a] -> Range a
+unions = foldl' (\r -> \x -> union x r) empty
+
+
+
+
+
+-- | Gets the nth element from the lower bound of a range, of the values within a range
+getNth :: (Ord a, Enum a) => Int -> Range a -> a
+getNth i (Range sps len)
+  | i >= len  = error "Out of range."
+  | otherwise = getNth' i sps
+  where
+    getNth' i (x@(Span _ _ l):xs)
+      | i >= l    = getNth' (i - l) xs
+      | otherwise = getNthOfSpan i x
